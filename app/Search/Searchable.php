3e2b68c7b3b7273f06367b50b5eb5e20f76e0bf9
Tony Messias <tonyzrp@gmail.com>
2016-12-17 13:41:05 -0300

    Add comment about feature flag

--- app/Search/Searchable.php
+++ app/Search/Searchable.php
@@ -6,6 +6,9 @@ trait Searchable
 {
     public static function bootSearchable()
     {
+        // We could check some config to see if elasticsearch is enabled
+        // here. It is useful when in test mode or using some kind of
+        // feature flags and you have to turn off your ES cluster.
         static::observe(app(ElasticsearchObserver::class));
     }
 
